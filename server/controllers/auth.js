8035866aa9556dbf8719e30f6a6f69f12b4c67b7
Doris5 <rmajchrzak7@o2.pl>
2023-07-27 23:49:21 +0200

    Setup user routes

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -28,7 +28,7 @@ export const register = async (req, res) => {
             friends,
             location,
             occupation,
-            viewedProfile: Math.floor(Math.random() *10000),
+            viewedProfile: Math.floor(Math.random() * 10000),
             impressions: Math.floor(Math.random() * 10000),
         });
 
@@ -38,3 +38,21 @@ export const register = async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 }
+
+// LOGGING IN
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email: email });
+        if (!user) return res.status(400).json({ msg: "User does not exist. " });
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) return res.status(400).json({ msg: "Invalid credentials. " });
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+        delete user.password;
+        res.status(200).json({ token, user });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
