7a2f4880b0dde4551ef29689d6dea456e7678e42
Doris5 <rmajchrzak7@o2.pl>
2023-07-28 22:23:48 +0200

    Add backend data

--- server/index.js
+++ server/index.js
@@ -9,8 +9,11 @@ import morgan from "morgan";
 import path from "path";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
 import { fileURLToPath } from "url";
 import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
 
 // CONFIGURATION, type=module
 const __filename = fileURLToPath(import.meta.url);
@@ -39,10 +42,12 @@ const upload = multer({ storage });
 
 // ROUTES WITH FILES
 app.post("/auth/register", upload.single("picture"), register);
+app.post("/post", verifyToken, upload.single("picture"), createPost)
 
 // ROUTES
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 // MONGOOSE SETUP
 const PORT = process.env.PORT || 6001;
